fd3b02546ef282915f1efb4f77fbb0e240e05749
Abdallah-55 <112090570+Abdallah-55@users.noreply.github.com>
2024-06-14 15:08:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const botConfigs = [
     host: 'donutsmp.net',
     username: 'yossefman123@gmail.com',
     auth: 'microsoft'    
+  },
+  {
+    host: 'donutsmp.net',
+    username: 'malekahmed2006@outlook.com',
+    auth: 'microsoft'    
   }
   /*{
     host: 'T2eelYaBoody.aternos.me',
